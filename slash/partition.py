a7e2c1cd8b7a3487b0d008abea20e959c20cfbc8
mirok <miroslav.kuchta@gmail.com>
2021-08-25 10:57:51 +0200

    partitioning into subdomains

--- slash/partition.py
+++ slash/partition.py
@@ -1,4 +1,6 @@
+from slash.embedded_mesh import EmbeddedMesh
 from collections import defaultdict, namedtuple
+from itertools import takewhile
 import dolfin as df
 import numpy as np
 import pymetis
@@ -22,7 +24,7 @@ def mesh2graph(mesh):
     return adj_list
 
 
-def partition(mesh, nsubs):
+def partition(mesh, nsubs, with_subd_meshes=False):
     '''
     Color mesh by mesh partitioner in `nsubs` subdomains. Return cell function
     colored by subdomains, facet function marking interfaces and lookup 
@@ -36,14 +38,91 @@ def partition(mesh, nsubs):
     coloring = np.array(coloring)
     ncolors = len(np.unique(coloring))
 
-    cell_f = df.MeshFunction('size_t', mesh, mesh.topology().dim(), 1)
+    tdim = mesh.topology().dim()
+    cell_f = df.MeshFunction('size_t', mesh, tdim, 1)
     values = cell_f.array()
     for color in range(ncolors):
         values[coloring == color] = color + 1
 
     interfaces = _find_interfaces(cell_f, ncolors)
+
+    if not with_subd_meshes:
+        return cell_f, interfaces
+
+    # Some more work left to do if we want submeshes. We actually want
+    # on each subdomain a facet function which has the right color for
+    # the interfaces. If the subdomain has only exterior boundary it
+    # will take colors from the subdomain color and then we are done
+    # so start there
+    subdomains, lc2lf_maps, gc2lc_maps = {}, {}, {}
     
-    return cell_f, interfaces
+    for subd_color in set(sum(interfaces.lookup.values(), ())):
+        subdomain = EmbeddedMesh(cell_f, subd_color)
+                
+        sub_facet_f = df.MeshFunction('size_t', subdomain, subdomain.topology().dim()-1, 0)
+        subdomains[subd_color] = sub_facet_f
+                
+        # We paint boundary with subdomain color for this is the
+        # right one on exterior iface; it changes only on the interior.
+        df.DomainBoundary().mark(sub_facet_f, subd_color)
+
+        _, f2c_loc = subdomain.init(tdim-1, tdim), subdomain.topology()(tdim-1, tdim) 
+        # cell -> its facets that are boundary
+        lc2lf = defaultdict(list)
+        for f in np.where(sub_facet_f.array() == subd_color)[0]:
+            [lc2lf[c].append(f) for c in f2c_loc(f)]
+        lc2lf_maps[subd_color] = lc2lf
+                
+        # Invert parent mapping of subdomain mesh
+        gc2lc_maps[subd_color] = {
+            v: k for k, v in subdomain.parent_entity_map[mesh.id()][tdim].items()
+        }
+
+    # No we need to fix coloring of the shared interior boundaries
+    facet_f, lookup = interfaces.facet_f, interfaces.lookup
+    ilookup = {v: k for k, v in lookup.items()}
+
+    _, f2c = mesh.init(tdim-1, tdim), mesh.topology()(tdim-1, tdim)
+    facets = facet_f.array()
+    
+    keys = iter(sorted(ilookup.keys(), key=len, reverse=True))
+
+    for subd_colors in takewhile(lambda key: len(key) == 2, keys):
+        bdry_color = ilookup[subd_colors]
+        bdry_facets, = np.where(facets == bdry_color)
+
+        for subd_color in subd_colors:
+            # Get the mappings ...
+            gc2lc_map, lc2lf_map = gc2lc_maps[subd_color], lc2lf_maps[subd_color]
+            # ... and domain we want to color
+            subdomain_facets = subdomains[subd_color]
+            sub_mesh = subdomain_facets.mesh()
+            
+            # gf -> gc -> lc -> lf
+            #
+            for gf in bdry_facets:
+                for gc in f2c(gf):
+                    # Cell in other domain?
+                    if gc not in gc2lc_map:
+                        continue
+                    
+                    lfs = lc2lf_map[gc2lc_map[gc]]
+                    # If cell had only one facet we have a match
+                    if len(lfs) == 1:
+                        lf = lfs[0]
+                    # We compare in terms of midpoints
+                    else:
+                        gm = df.Facet(mesh, gf).midpoint()
+                        lfs = [df.Facet(sub_mesh, lfi) for lfi in lfs]
+                        lf = min(lfs, key=lambda l: l.midpoint().distance(gm))
+
+                        assert lf.midpoint().distance(gm) < 1E-13
+
+                        lf = lf.index()
+                    # Colors        
+                    subdomain_facets[lf] = bdry_color
+                    
+    return cell_f, interfaces, subdomains
 
 
 def _find_interfaces(cell_f, nsubs):
@@ -79,18 +158,18 @@ def _find_interfaces(cell_f, nsubs):
     lookup = {}
     
     next_color = 1
-    for key in sorted(interfaces):
+    for key in sorted(interfaces, key=len):
+
         # Exterior get color of the subdomain
         if len(key) == 1:
             color = key[0]
         # Interior is first come first serve
         else:
             color = next_color
-        
         values[interfaces[key]] = color
+
         lookup[color] = key
-        
-        next_color += 1
+        next_color += 1                    
 
     return namedtuple('Interface', ('facet_f', 'lookup'))(facet_f, lookup)
 
@@ -99,12 +178,15 @@ def _find_interfaces(cell_f, nsubs):
 if __name__ == '__main__':
 
     
-    mesh = df.UnitSquareMesh(32, 32)
+    mesh = df.UnitSquareMesh(128, 128)
 
-    # from gmshnics import gUnitSquare
-    # mesh, _ = gUnitSquare(0.2)
+    #from gmshnics import gUnitSquare
+    #mesh, _ = gUnitSquare(0.2)
 
-    color_f, interface = partition(mesh, 3)
+    color_f, interface, subdomains = partition(mesh, 4, with_subd_meshes=True)
 
     df.File('foo.pvd') << color_f
     df.File('bar.pvd') << interface.facet_f
+    print(interface.lookup)
+    for k, v in subdomains.items():
+        df.File(f'subd_{k}.pvd') << v
